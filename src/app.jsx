3d944d53b6517c5a868a4dfc8aa0653f260a154b
MatheusZamo <19matheushenrique03@gmail.com>
2024-10-16 11:33:36 -0300

    Base Jsx and Css code

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,22 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const App = () => (
+  <>
+    <input type="number" autoFocus />
+
+    <div className="selects">
+      <select>
+        <option value="BRL">BRL</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+      </select>
+      <select>
+        <option value="BRL">BRL</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+      </select>
+    </div>
+
+    <h2>4.87 BRL</h2>
+  </>
+)
 
 export { App }
